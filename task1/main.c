f709878281f72d7b9312d5311698dcef3f8748ad
dev3490 <223674555+dev3490@users.noreply.github.com>
2025-10-16 18:40:03 +0400

    first solution

--- task1/main.c
+++ task1/main.c
@@ -1,3 +1,26 @@
-int main(void) {
-    // Тут могло быть ваше решение.
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int argc, char* argv[]) {
+  uintmax_t char_count = 0,
+            word_count = 0,
+            line_count = 0;
+
+  FILE* file = argc > 1 ? fopen(argv[1], "r") : stdin;
+  if (!file) {
+      perror("File opening failed!!! Achtung!!!");
+      return 1;
+  }
+
+  char c;
+  while((c = fgetc(file)) != EOF) {
+    switch (c) {
+      case '\n':                                 ++line_count; __attribute__((fallthrough));
+      case '\t': case '\v': case '\r': case ' ': ++word_count;
+    }
+    ++char_count;
+    if (ferror(file)) puts("I/O error when reading");
+  }
+
+  printf("%ju\n%ju\n%ju\n", line_count, word_count, char_count);
 }
