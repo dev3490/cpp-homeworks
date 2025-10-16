58d147b2807cdf3d3f87709c9e6c463788c646a1
dev3490 <223674555+dev3490@users.noreply.github.com>
2025-10-16 20:28:23 +0400

    fix: error handling - change type of "c" to int because EOF is int - move ferror() check outside the cycle - replace all exit codes to EXIT_FAILURE/SUCCESS

--- task1/main.c
+++ task1/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
   uintmax_t char_count = 0,
@@ -8,19 +9,24 @@ int main(int argc, char* argv[]) {
 
   FILE* file = argc > 1 ? fopen(argv[1], "r") : stdin;
   if (!file) {
-      perror("File opening failed!!! Achtung!!!");
-      return 1;
+    perror("File opening failed!!! Achtung!!!");
+    return EXIT_FAILURE;
   }
 
-  char c;
+  int c;
   while((c = fgetc(file)) != EOF) {
     switch (c) {
       case '\n':                                 ++line_count; __attribute__((fallthrough));
       case '\t': case '\v': case '\r': case ' ': ++word_count;
     }
     ++char_count;
-    if (ferror(file)) puts("I/O error when reading");
+  }
+
+  if (ferror(file)) {
+    fprintf(stderr, "I/O error when reading");
+    return EXIT_FAILURE;
   }
 
   printf("%ju\n%ju\n%ju\n", line_count, word_count, char_count);
+  return EXIT_SUCCESS;
 }
